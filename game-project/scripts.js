c2748e0315fbd7520623167b98ecd1a62038c036
KyLLlHEP <kushnerartur@gmail.com>
2023-12-09 14:48:00 +0000

    feat: game done witout random picture

--- game-project/scripts.js
+++ game-project/scripts.js
@@ -1,29 +1,38 @@
-//Get elem memory-cards
+// Получаем элементы карт
 const cards = document.querySelectorAll(".memory-card");
-// manipulation cards
+
 let hasFlippedCard = false;
+let lockBoard = false; // Блокировка доски
 let firstCard, secondCard;
 
-// Flipped card
+// Функция для переворота карты
 function flipCard() {
+  if (lockBoard) return; // Предотвращаем переворачивание, если доска заблокирована
+  if (this === firstCard) return; // Предотвращаем двойной клик на одну и ту же карту
+
   this.classList.add("flip");
+
   if (!hasFlippedCard) {
-    //first click
+    // Первый клик
     hasFlippedCard = true;
     firstCard = this;
   } else {
-    //second click
+    // Второй клик
     hasFlippedCard = false;
     secondCard = this;
-    //Match card
+
+    // Проверяем, совпадают ли карты
     if (firstCard.dataset.pictures === secondCard.dataset.pictures) {
-      //It's a match
+      // Карты совпадают
       firstCard.removeEventListener("click", flipCard);
       secondCard.removeEventListener("click", flipCard);
     } else {
+      // Карты не совпадают, закрываем их
+      lockBoard = true; // Блокируем доску
       setTimeout(() => {
         firstCard.classList.remove("flip");
         secondCard.classList.remove("flip");
+        lockBoard = false; // Разблокируем доску
       }, 1500);
     }
   }
