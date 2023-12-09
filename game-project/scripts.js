f26406499e38ffb081be802e07d140e67de9ab4b
KyLLlHEP <kushnerartur@gmail.com>
2023-12-09 11:32:10 +0000

    feat: add date-picture each memory-card

--- game-project/scripts.js
+++ game-project/scripts.js
@@ -1,6 +1,21 @@
+//Get elem memory-cards
 const cards = document.querySelectorAll(".memory-card");
+// manipulation cards
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+// Flipped card
 function flipCard() {
-  this.classList.toggle("flip");
+  this.classList.add("flip");
+  if (!hasFlippedCard) {
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
+  }
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
