b971459931eefb6f3e4934fb10819bd381d06878
KyLLlHEP <kushnerartur@gmail.com>
2023-12-09 15:51:50 +0000

    feat: add random image and not double pars same picture

--- create.js
+++ create.js
@@ -17,19 +17,11 @@ export const images = [
   "Top hat.jpg",
   "Umbrella.jpg",
 ];
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
 export function generateCards(level, gameBoard) {
   const pairs = level;
-  let selectedImages = shuffle(images.slice(0, pairs));
-  selectedImages = [...selectedImages, ...selectedImages];
-  selectedImages = shuffle(selectedImages);
+  let selectedImages = getRandomImages(images, pairs);
+  selectedImages = [...selectedImages, ...selectedImages]; // Дублируем для создания пар
+  selectedImages = shuffle(selectedImages); // Перемешиваем картинки
 
   gameBoard.innerHTML = "";
 
@@ -43,3 +35,38 @@ export function generateCards(level, gameBoard) {
     gameBoard.innerHTML += cardHTML;
   });
 }
+
+// Функция для выбора случайных уникальных изображений
+function getRandomImages(images, count) {
+  let shuffledImages = shuffle([...images]);
+  return shuffledImages.slice(0, count);
+}
+
+// Функция shuffle остается прежней
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+// export function generateCards(level, gameBoard) {
+//   const pairs = level;
+//   let selectedImages = shuffle(images.slice(0, pairs));
+//   selectedImages = [...selectedImages, ...selectedImages];
+//   selectedImages = shuffle(selectedImages);
+
+//   gameBoard.innerHTML = "";
+
+//   selectedImages.forEach((img) => {
+//     const cardHTML = `
+//       <div class="memory-card" data-pictures="${img}">
+//         <img class="front-face" src="img/${img}" alt="${img}" />
+//         <img class="back-face" src="img/Back-face.png" alt="memory-card" />
+//       </div>
+//     `;
+//     gameBoard.innerHTML += cardHTML;
+//   });
+// }
